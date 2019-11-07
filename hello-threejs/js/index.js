3162f19a258aca34b9d35cd312387a076881aa11
amaisuna <taro30143014@gmail.com>
2019-11-08 06:01:13 +0900

    create hello-threejs

--- hello-threejs/js/index.js
+++ hello-threejs/js/index.js
@@ -6,19 +6,19 @@ function init() {
     let camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
     // レンダーの設定
     let renderer = new THREE.WebGLRenderer();
-    
+
     // renderer.setClearColorHex();
     renderer.setClearColor(new THREE.Color(0xEEEEEE));
     renderer.setSize(window.innerWidth, window.innerHeight);
 
     // 画面に(x, y, z)軸を表示
-    var axes = new THREE.AxisHelper(20);
+    let axes = new THREE.AxisHelper(20);
     scene.add(axes);
 
-    // 平面を作成
-    var planeGeometry = new THREE.PlaneGeometry(60, 20);
-    var planeMaterial = new THREE.MeshBasicMaterial({color: 0xcccccc});
-    var plane = new THREE.Mesh(planeGeometry, planeMaterial);
+    // 平面を定義
+    let planeGeometry = new THREE.PlaneGeometry(60, 20);
+    let planeMaterial = new THREE.MeshBasicMaterial({color: 0xcccccc});
+    let plane = new THREE.Mesh(planeGeometry, planeMaterial);
 
     // 平面のポジション
     plane.rotation.x = -0.5 * Math.PI;
@@ -27,10 +27,10 @@ function init() {
     plane.position.z = 0;
     scene.add(plane);
 
-    // 立方体を作成
-    var cubeGeometry = new THREE.BoxGeometry(4, 4, 4);
-    var cubeMaterial = new THREE.MeshBasicMaterial({color: 0xff0000, wireframe: true});
-    var cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
+    // 立方体を定義
+    let cubeGeometry = new THREE.BoxGeometry(4, 4, 4);
+    let cubeMaterial = new THREE.MeshBasicMaterial({color: 0xff0000, wireframe: true});
+    let cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
 
     // 立方体のポジション
     cube.position.x = -4;
@@ -38,10 +38,10 @@ function init() {
     cube.position.z = 0;
     scene.add(cube);
 
-    // 球体を作成
-    var sphereGeometry = new THREE.SphereGeometry(4, 20, 20);
-    var sphereMaterial = new THREE.MeshBasicMaterial({color: 0x7777ff, wireframe: true});
-    var sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
+    // 球体を定義
+    let sphereGeometry = new THREE.SphereGeometry(4, 20, 20);
+    let sphereMaterial = new THREE.MeshBasicMaterial({color: 0x7777ff, wireframe: true});
+    let sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
 
     // 球体のポジション
     sphere.position.x = 20;
@@ -53,6 +53,7 @@ function init() {
     camera.position.x = -30;
     camera.position.y = 40;
     camera.position.z = 30;
+    // lookAt()でシーンの中心を向く
     camera.lookAt(scene.position);
 
     // シーンをHTMLにレンダリング
